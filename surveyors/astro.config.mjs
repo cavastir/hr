6e436ba758bad5f286b2c512a488fe552df0b60f
cavastir <pat@cavastir.com>
2024-10-05 21:18:16 +0700

    removed all react components at least home page ... no more framer-motion etc.

--- surveyors/astro.config.mjs
+++ surveyors/astro.config.mjs
@@ -1,21 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [tailwind()],
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
     }
   },
   vite: {
-    ssr: {
-      noExternal: ['react', 'react-dom', 'framer-motion'],
-    },
-    optimizeDeps: {
-      exclude: ['problematic-package'],
-    },
+    plugins: [
+      {
+        name: 'virtual-module',
+        resolveId(id) {
+          if (id.startsWith('virtual:')) {
+            return id;
+          }
+        },
+        load(id) {
+          if (id.startsWith('virtual:')) {
+            return ''; // Return an empty string for virtual modules
+          }
+        },
+      },
+    ],
   }
 });
