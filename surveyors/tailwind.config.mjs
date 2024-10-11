c4b632fed75a63005a8e060d628a1800bdffb75b
cavastir <pat@cavastir.com>
2024-10-11 19:52:43 +0700

    playing with tailwind config

--- surveyors/tailwind.config.mjs
+++ surveyors/tailwind.config.mjs
@@ -36,6 +36,16 @@ export default {
 			borderRadius: {
 				'custom': '6px',
 			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '4rem',
+					xl: '5rem',
+					'2xl': '6rem',
+				},
+			},
 		},
 	},
 	plugins: [],
