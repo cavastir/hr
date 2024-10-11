bebbfd33900fa60d0030b67e6621514b1495723f
cavastir <pat@cavastir.com>
2024-10-11 19:43:13 +0700

    rebuilt

--- surveyors/astro.config.mjs
+++ surveyors/astro.config.mjs
@@ -1,43 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto'
   },
   vite: {
-    plugins: [
-      {
-        name: 'virtual-module',
-        resolveId(id) {
-          if (id.startsWith('virtual:')) {
-            return id;
-          }
-        },
-        load(id) {
-          if (id.startsWith('virtual:')) {
-            return ''; // Return an empty string for virtual modules
-          }
-        },
-      },
-    ],
     build: {
       rollupOptions: {
         output: {
-          entryFileNames: 'entry.[hash].js',
-          chunkFileNames: 'chunks/chunk.[hash].js',
-          assetFileNames: 'assets/asset.[hash][extname]',
+          manualChunks: undefined,
         },
       },
     },
   },
-  output: 'static',
-  build: {
-    inlineStylesheets: 'auto'
-  }
 });
