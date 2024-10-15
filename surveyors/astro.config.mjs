15770d7f4bb7bb8de8c928e0be5ef085e7c8b81a
cavastir <pat@cavastir.com>
2024-10-15 20:08:55 +0700

    added robots.txt and installed astro sitemaps

--- surveyors/astro.config.mjs
+++ surveyors/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   build: {
     inlineStylesheets: 'auto'
@@ -16,4 +17,5 @@ export default defineConfig({
       },
     },
   },
+  site: 'https://hrsurveyors.com.au',
 });
