67c1231f162c1fb37b41543da3ce5585e5048de4
cavastir <pat@cavastir.com>
2024-11-22 09:56:26 +0700

    quick redirect fixes

--- surveyors/astro.config.mjs
+++ surveyors/astro.config.mjs
@@ -5,18 +5,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   output: 'static',
+  site: 'https://www.hrsurveyors.com.au',
+  trailingSlash: 'always',
   build: {
     inlineStylesheets: 'auto'
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: undefined,
-        },
-      },
-    },
-  },
-  site: 'https://www.hrsurveyors.com.au',
-  trailingSlash: 'always', // Ensures all URLs have a trailing slash
+  }
 });
