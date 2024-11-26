3419dc65f3197e9f63e7306bd2b08f325d8d0174
cavastir <pat@cavastir.com>
2024-11-26 10:05:21 +0700

    fix sitemap.xml returning 404

--- surveyors/astro.config.mjs
+++ surveyors/astro.config.mjs
@@ -3,7 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ['https://www.hrsurveyors.com.au'],
+      filter: (page) => !page.includes('/404'),
+      entryLimit: 10000, // Number of URLs per sitemap file
+    })
+  ],
   output: 'static',
   site: 'https://www.hrsurveyors.com.au',
   trailingSlash: 'always',
